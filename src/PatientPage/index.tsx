52eade8489fa109bae3410d313c356d9b8b5a311
Laura Perttilä <laura@perttila.me>
2021-08-12 11:21:46 +0300

    exercise 9.20

--- src/PatientPage/index.tsx
+++ src/PatientPage/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import { apiBaseUrl } from '../constants';
-import { useStateValue } from "../state";
+import { useStateValue, updatePatient } from "../state";
 import {Patient} from '../types';
 import GenderIcon from './GenderIcon';
 
@@ -18,7 +18,7 @@ const PatientPage = () => {
         const fetchPatient = async () => {
             try {
                 const {data: patientFromApi} = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
-                dispatch({type: "UPDATE_PATIENT", payload: patientFromApi});
+                dispatch(updatePatient(patientFromApi));
             } catch (error) {
                 throw new Error(error.message);
             }
@@ -36,6 +36,32 @@ const PatientPage = () => {
                 </div>
                 <div>
                     occupation: {patients[id].occupation}
+                </div>
+                <h3>Entries</h3>
+                <div>
+                    {patients[id].entries.map(entry => {
+                        if (entry.diagnosisCodes !== undefined) {
+                            return (
+                                <div>
+                                    <div>
+                                        {entry.date} {entry.description}
+                                    </div>
+                                    <div>
+                                        <ul>
+                                        {entry.diagnosisCodes.map(code => (
+                                        <li key={code}>{code}</li>
+                                        ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            ); 
+                        }
+                        return (
+                            <div key={entry.id}>
+                                {entry.date} {entry.description}
+                            </div>
+                        );                       
+                    })}
                 </div>
             </div>
             }
